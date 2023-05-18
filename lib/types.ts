f90c823a6d30d13fe6bfd8756d15642e1d29e4b0
hasundue <hasundue@gmail.com>
2023-05-18 23:00:12 +0900

    wip

--- lib/types.ts
+++ lib/types.ts
@@ -7,3 +7,9 @@ export type NostrEvent = Brand<string, "NostrEvent">;
 export type NostrPubkey = Brand<string, "NostrPubkey">;
 
 export type RelayUrl = `wss://${string}`;
+
+type WebSocketEventType = Omit<WebSocketEventMap, "message">;
+
+export type WebSocketEventListner = {
+  [K in keyof WebSocketEventType]: (event: WebSocketEventMap[K]) => void;
+};
--- relay.ts
+++ relay.ts
@@ -1,8 +1,8 @@
-import { log } from "./lib/log.ts";
 import { Notify } from "https://deno.land/x/async@v2.0.2/notify.ts";
 import { Event as NostrEvent, Filter } from "npm:nostr-tools@1.10.1";
-import { RelayUrl } from "./lib/types.ts";
-import { Brand, noop, now } from "./lib/utils.ts";
+import { Brand, noop } from "./lib/utils.ts";
+import { log } from "./lib/log.ts";
+import { RelayUrl, WebSocketEventListner } from "./lib/types.ts";
 
 export interface RelayConfig {
   name?: string;
@@ -18,7 +18,128 @@ export type RelayToClientEventListener = Partial<
   & RelayToClientMessageListener
 >;
 
-interface SubscribeOptions {
+export class Relay {
+  #ws: WebSocket;
+  readonly name: string;
+  readonly #notifier = new Notify();
+  readonly #subscriptions = new Map<SubscriptionId, SubscriptionProvider>();
+
+  constructor(public readonly config: RelayConfig) {
+    this.#ws = this.connect();
+    this.name = this.config.name ?? this.config.url;
+  }
+
+  connect(): WebSocket {
+    const ws = new WebSocket(this.config.url);
+
+    ws.onopen = (event) => {
+      log.info("connection opened", this.name);
+      this.config.on?.open?.call(this, event);
+      this.#notifier.notify();
+    };
+
+    ws.onclose = (event) => {
+      log.info("connection closed", this.name);
+      (event.code === 1000 ? log.debug : log.error)(event);
+      this.config.on?.close?.call(this, event);
+      this.#notifier.notify();
+    };
+
+    ws.onerror = (event) => {
+      log.error("connection error", this.name);
+      this.config.on?.error?.call(this, event);
+    };
+
+    ws.onmessage = (ev: MessageEvent<string>) => {
+      log.debug(ev.data);
+      const msg = JSON.parse(ev.data) as RelayToClientMessage;
+      try {
+        switch (msg[0]) {
+          case "EVENT": {
+            const [, sid, event] = msg;
+            this.config.on?.event?.call(this, sid, event);
+            this.#subscription(sid)?.write(event);
+            break;
+          }
+          case "EOSE": {
+            const [, sid] = msg;
+            this.config.on?.eose?.call(this, sid);
+            if (this.config.close_on_eose) {
+              this.#subscription(sid)?.close();
+            }
+            break;
+          }
+          case "NOTICE": {
+            const [, message] = msg;
+            this.config.on?.notice?.call(this, message);
+            break;
+          }
+          default:
+            log.warning("Unknown message type:", msg[0]);
+        }
+      } catch (err) {
+        log.error(err);
+      }
+    };
+    return ws;
+  }
+
+  close() {
+    this.#subscriptions.forEach((sub) => sub.close());
+    this.#ws.close();
+  }
+
+  async ensureOpen(onopen: (relay: Relay) => void = noop) {
+    switch (this.#ws.readyState) {
+      case WebSocket.CONNECTING:
+        await this.#notifier.notified();
+        onopen(this);
+        /* falls through */
+
+      case WebSocket.OPEN:
+        return;
+
+      case WebSocket.CLOSING:
+        await this.#notifier.notified();
+        /* falls through */
+
+      case WebSocket.CLOSED:
+        this.#ws = this.connect();
+        await this.#notifier.notified();
+        onopen(this);
+    }
+  }
+
+  async send(message: ClientToRelayMessage) {
+    await this.ensureOpen();
+    this.#ws.send(JSON.stringify(message));
+    log.debug("Sent", message, this.name);
+  }
+
+  subscribe(filter: Filter, options: SubscribeOptions = {}): Subscription {
+    const sub = new SubscriptionProvider(filter, options, this);
+    this.#subscriptions.set(sub.id, sub);
+    return sub;
+  }
+
+  unsubscribe(id: SubscriptionId) {
+    this.#subscriptions.delete(id);
+  }
+
+  #subscription(id: SubscriptionId) {
+    const sub = this.#subscriptions.get(id);
+    if (!sub) {
+      log.warning("Unknown subscription", id);
+      this.send(["CLOSE", id]);
+    }
+    return sub;
+  }
+}
+
+//
+// Subscription and SubscriptionProvider
+//
+export interface SubscribeOptions {
   id?: string;
 }
 
@@ -85,128 +206,9 @@ class SubscriptionProvider extends TransformStream<NostrEvent, NostrEvent> {
   }
 }
 
-export class Relay {
-  #ws: WebSocket;
-  readonly name: string;
-  readonly #notifier = new Notify();
-  readonly #subscriptions = new Map<SubscriptionId, SubscriptionProvider>();
-
-  constructor(public readonly config: RelayConfig) {
-    this.name = this.config.name ?? this.config.url;
-    this.#ws = this.connect();
-  }
-
-  connect(): WebSocket {
-    const ws = new WebSocket(this.config.url);
-
-    ws.onopen = (event) => {
-      log.info("connection opened", this.name);
-      this.config.on?.open?.call(this, event);
-      this.#notifier.notify();
-    };
-
-    ws.onclose = (event) => {
-      log.info("connection closed", this.name);
-      (event.code === 1000 ? log.debug : log.error)(event);
-      this.config.on?.close?.call(this, event);
-      this.#notifier.notify();
-    };
-
-    ws.onerror = (event) => {
-      log.error("connection error", this.name);
-      this.config.on?.error?.call(this, event);
-    };
-
-    ws.onmessage = async (ev: MessageEvent<string>) => {
-      log.debug(ev.data);
-      const msg = JSON.parse(ev.data) as RelayToClientMessage;
-      try {
-        switch (msg[0]) {
-          case "EVENT": {
-            const [, sid, event] = msg;
-            if (this.config.on?.event) this.config.on.event(sid, event);
-
-            const sub = this.#subscriptions.get(sid);
-            if (!sub) {
-              log.warning("Unknown subscription", event);
-              this.send(["CLOSE", sid]);
-              break;
-            }
-            await sub.write(event);
-            break;
-          }
-          case "EOSE": {
-            const [, sid] = msg;
-            this.config.on?.eose?.call(this, sid);
-
-            if (this.config.close_on_eose) {
-              this.close();
-            }
-            break;
-          }
-          case "NOTICE": {
-            const [, message] = msg;
-            this.config.on?.notice?.call(this, message);
-
-            break;
-          }
-          default:
-            log.warning("Unknown message type:", msg[0]);
-        }
-      } catch (err) {
-        log.error(err);
-      }
-    };
-    return ws;
-  }
-
-  close() {
-  }
-
-  async ensureOpen(onopen: (relay: Relay) => void = noop) {
-    switch (this.#ws.readyState) {
-      case WebSocket.CONNECTING:
-        await this.#notifier.notified();
-        onopen(this);
-        /* falls through */
-
-      case WebSocket.OPEN:
-        return;
-
-      case WebSocket.CLOSING:
-        await this.#notifier.notified();
-        /* falls through */
-
-      case WebSocket.CLOSED:
-        this.#ws = this.connect();
-        await this.#notifier.notified();
-        onopen(this);
-    }
-  }
-
-  async send(message: ClientToRelayMessage) {
-    await this.ensureOpen();
-    this.#ws.send(JSON.stringify(message));
-    log.debug("Sent", message, this.name);
-  }
-
-  subscribe(filter: Filter, options: SubscribeOptions = {}): Subscription {
-    const sub = new SubscriptionProvider(filter, options, this);
-    this.#subscriptions.set(sub.id, sub);
-    return sub;
-  }
-
-  unsubscribe(id: SubscriptionId) {
-    this.#subscriptions.delete(id);
-  }
-}
-
-type WebSocketEventType = Omit<WebSocketEventMap, "message">;
-
-type WebSocketEventListner = {
-  [K in keyof WebSocketEventType]: (event: WebSocketEventMap[K]) => void;
-};
-
+//
+// Types
+//
 type RelayToClientMessageListener = {
   "event": (id: SubscriptionId, event: NostrEvent) => void;
   "eose": (id: SubscriptionId) => void;
@@ -224,98 +226,3 @@ type ClientToRelayMessage =
   | ["CLOSE", SubscriptionId];
 
 type SubscriptionId = Brand<string, "SubscriptionId">;
-
-export class RelayPool {
-  private relays: Relay[];
-
-  constructor(relays: RelayConfig[]) {
-    this.relays = relays.map((config) => new Relay(config));
-  }
-
-  connect(relays: RelayConfig) {
-    this.relays.push(new Relay(relay));
-  }
-
-  private async reconnect(relay: Relay) {
-    await relay.reconnect();
-    // Restart all subscriptions to the relay.
-    if (relay.read) {
-      this.subs.forEach((sub) => sub.restart(relay));
-    }
-  }
-
-  subscribe(filter: Filter, options?: SubscribeOptions) {
-    return new SubscriptionProvider(
-      this.relays.filter((conn) => conn.read),
-      { since: now(), ...filter },
-      options,
-    );
-  }
-
-  retrieve(filter: Filter, options?: SubscribeOptions) {
-    const sub = new SubscriptionProvider(
-      this.relays.filter((conn) => conn.read),
-      filter,
-      { close_on_eose: true, ...options },
-    );
-    return sub.stream;
-  }
-
-  async getLatest(filter: Filter): Promise<Event | null> {
-    const sub = new SubscriptionProvider(
-      this.relays.filter((conn) => conn.read),
-      { limit: 1, ...filter },
-      { close_on_eose: true },
-    );
-    for await (const event of sub.stream) {
-      return event;
-    }
-    return null;
-  }
-
-  async publish(event: Event) {
-    const env = Deno.env.get("RAILWAY_ENVIRONMENT");
-
-    if (env !== "production") {
-      console.log(`Skipped publishing (env: ${env}).`);
-      return;
-    }
-
-    await Promise.all(
-      this.relays.filter((conn) => conn.write).map(async (relay) => {
-        if (!relay.connected) {
-          console.assert(
-            !relay.read,
-            "Non write-only relay is left disconnected:",
-            relay.url,
-          );
-          await this.reconnect(relay);
-        }
-
-        const sub = this.subscribe({ ids: [event.id] });
-        const pub = relay.publish(event);
-
-        pub.on("failed", (reason: string) => {
-          console.error(
-            `Failed to publish an event ${event.id} to ${relay.url}:`,
-            { cause: reason },
-          );
-        });
-
-        // Wait for the event to be published.
-        for await (const event of sub.stream) {
-          console.log(
-            `Event ${event.id} has been published to ${relay.url}.`,
-          );
-          return;
-        }
-      }),
-    );
-  }
-
-  close() {
-    this.relays.forEach((relay) => {
-      relay.close();
-    });
-  }
-}
