d24fa52afbb7256deb3d6f94a92e8aab1afc5253
hasundue <hasundue@gmail.com>
2023-05-19 06:17:15 +0900

    refactor

--- lib/types.ts
+++ lib/types.ts
@@ -1,13 +1,31 @@
 import { Brand } from "./utils.ts";
+import { Event as NostrEvent, Filter } from "npm:nostr-tools@1.10.1";
 
+//
+// Events
+//
 export type EventId = Brand<string, "EventId">;
+export type SubscriptionId = Brand<string, "SubscriptionId">;
 
-export type NostrProfile = Brand<string, "NostrProfile">;
-export type NostrEvent = Brand<string, "NostrEvent">;
-export type NostrPubkey = Brand<string, "NostrPubkey">;
+//
+// Communication
+//
+export type WebSocketUrl = `wss://${string}`;
+export type RelayUrl = Brand<WebSocketUrl, "RelayUrl">;
 
-export type RelayUrl = `wss://${string}`;
+export type RelayToClientMessage =
+  | ["EVENT", SubscriptionId, NostrEvent]
+  | ["EOSE", SubscriptionId, ...Filter[]]
+  | ["NOTICE", string];
 
+export type ClientToRelayMessage =
+  | ["EVENT", NostrEvent]
+  | ["REQ", SubscriptionId, ...Filter[]]
+  | ["CLOSE", SubscriptionId];
+
+//
+// WebSocket
+//
 export type WebSocketEventType = Omit<WebSocketEventMap, "message">;
 
 export type WebSocketEventListner = {
--- relay.ts
+++ relay.ts
@@ -1,16 +1,21 @@
 import { Notify } from "https://deno.land/x/async@v2.0.2/notify.ts";
 import { Event as NostrEvent, Filter } from "npm:nostr-tools@1.10.1";
-import { Brand } from "./lib/utils.ts";
 import { log } from "./lib/log.ts";
-import { RelayUrl, WebSocketEventListner } from "./lib/types.ts";
+import {
+  ClientToRelayMessage,
+  RelayToClientMessage,
+  RelayUrl,
+  SubscriptionId,
+  WebSocketEventListner,
+  WebSocketUrl,
+} from "./lib/types.ts";
 
 export interface RelayConfig {
   name?: string;
-  url: RelayUrl;
+  url: WebSocketUrl;
   read?: boolean;
   write?: boolean;
   on?: RelayToClientEventListener;
-  close_on_eose?: boolean;
 }
 
 export type RelayToClientEventListener = Partial<
@@ -19,35 +24,44 @@ export type RelayToClientEventListener = Partial<
 >;
 
 export class Relay {
-  #ws: WebSocket;
   readonly name: string;
+  readonly url: RelayUrl;
+  readonly read: boolean;
+  readonly write: boolean;
+  readonly on: RelayToClientEventListener;
+
+  #ws: WebSocket;
   readonly #notifier = new Notify();
   readonly #subscriptions = new Map<SubscriptionId, SubscriptionProvider>();
 
-  constructor(public readonly config: RelayConfig) {
+  constructor(config: RelayConfig) {
     this.#ws = this.connect();
-    this.name = this.config.name ?? this.config.url;
+    this.url = config.url as RelayUrl;
+    this.name = config.name ?? config.url;
+    this.read = config.read ?? true;
+    this.write = config.write ?? true;
+    this.on = config.on ?? {};
   }
 
   connect(): WebSocket {
-    const ws = new WebSocket(this.config.url);
+    const ws = new WebSocket(this.url);
 
     ws.onopen = (event) => {
       log.info("connection opened", this.name);
-      this.config.on?.open?.call(this, event);
+      this.on.open?.call(this, event);
       this.#notifier.notify();
     };
 
     ws.onclose = (event) => {
       log.info("connection closed", this.name);
       (event.code === 1000 ? log.debug : log.error)(event);
-      this.config.on?.close?.call(this, event);
+      this.on.close?.call(this, event);
       this.#notifier.notify();
     };
 
     ws.onerror = (event) => {
       log.error("connection error", this.name);
-      this.config.on?.error?.call(this, event);
+      this.on.error?.call(this, event);
     };
 
     ws.onmessage = (ev: MessageEvent<string>) => {
@@ -57,21 +71,20 @@ export class Relay {
         switch (msg[0]) {
           case "EVENT": {
             const [, sid, event] = msg;
-            this.config.on?.event?.call(this, sid, event);
+            this.on.event?.call(this, sid, event);
             this.#subscription(sid)?.write(event);
             break;
           }
           case "EOSE": {
             const [, sid] = msg;
-            this.config.on?.eose?.call(this, sid);
-            if (this.config.close_on_eose) {
-              this.#subscription(sid)?.close();
-            }
+            this.on.eose?.call(this, sid);
+            const sub = this.#subscription(sid);
+            if (sub?.options.close_on_eose) sub.close();
             break;
           }
           case "NOTICE": {
             const [, message] = msg;
-            this.config.on?.notice?.call(this, message);
+            this.on.notice?.call(this, message);
             break;
           }
           default:
@@ -117,6 +130,9 @@ export class Relay {
   }
 
   subscribe(filter: Filter, options: SubscribeOptions = {}): Subscription {
+    if (!this.read) {
+      throw new Error("Cannot subscribe to a write-only relay");
+    }
     const sub = new SubscriptionProvider(filter, options, this);
     this.#subscriptions.set(sub.id, sub);
     return sub;
@@ -141,6 +157,7 @@ export class Relay {
 //
 export interface SubscribeOptions {
   id?: string;
+  close_on_eose?: boolean;
 }
 
 export interface Subscription {
@@ -201,7 +218,7 @@ class SubscriptionProvider extends TransformStream<NostrEvent, NostrEvent> {
     writer.releaseLock();
   }
 
-  get stream() {
+  get events() {
     return this.readable;
   }
 }
@@ -214,15 +231,3 @@ type RelayToClientMessageListener = {
   "eose": (id: SubscriptionId) => void;
   "notice": (message: string) => void;
 };
-
-type RelayToClientMessage =
-  | ["EVENT", SubscriptionId, NostrEvent]
-  | ["EOSE", SubscriptionId, ...Filter[]]
-  | ["NOTICE", string];
-
-type ClientToRelayMessage =
-  | ["EVENT", NostrEvent]
-  | ["REQ", SubscriptionId, ...Filter[]]
-  | ["CLOSE", SubscriptionId];
-
-type SubscriptionId = Brand<string, "SubscriptionId">;
