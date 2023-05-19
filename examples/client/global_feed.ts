78f20b95a21290f4d6fcb8fdc10f83328bc4a418
hasundue <hasundue@gmail.com>
2023-05-19 14:01:34 +0900

    Rename test/client_test.ts to examples/client/global_feed.ts

---
+++ examples/client/global_feed.ts
@@ -0,0 +1,8 @@
+import { connect } from "../../client.ts";
+
+const relay = connect({ url: "wss://nos.lol" });
+const sub = relay.subscribe({ kinds: [1] });
+
+for await (const event of sub.events) {
+  console.log(event);
+}
--- test/client_test.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from "../client.ts";
-import { now } from "../lib/utils.ts";
-
-const relay = connect({ url: "wss://nos.lol" });
-const sub = relay.subscribe({ kinds: [1], since: now() });
-
-for await (const event of sub.events) {
-  console.log(event);
-}
