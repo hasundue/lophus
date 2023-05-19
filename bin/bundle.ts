ac50d5acc2918839fa75b99e07964fde97977934
hasundue <hasundue@gmail.com>
2023-05-19 13:05:29 +0900

    Add minifier in bundle.ts

--- bin/bundle.ts
+++ bin/bundle.ts
@@ -1,8 +1,14 @@
 import { bundle } from "https://deno.land/x/emit@0.22.0/mod.ts";
+import { transform } from "https://deno.land/x/esbuild@v0.17.19/mod.js";
 
 const result = await bundle(
   new URL("../client.ts", import.meta.url),
 );
-
 const { code } = result;
-console.log(code);
+
+const minified = await transform(code, {
+  minify: true,
+});
+
+console.log(minified.code);
+Deno.exit(0);
