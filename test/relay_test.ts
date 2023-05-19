7a079f7360b39979bfc9bd7450ebc618dde0df78
hasundue <hasundue@gmail.com>
2023-05-19 11:29:17 +0900

    Modify format

--- test/relay_test.ts
+++ test/relay_test.ts
@@ -1,14 +1,8 @@
 import { connect } from "../relay.ts";
 import { now } from "../lib/utils.ts";
 
-const relay = connect({
-  url: "wss://nos.lol",
-});
-
-const sub = relay.subscribe({
-  kinds: [1],
-  since: now(),
-});
+const relay = connect({ url: "wss://nos.lol" });
+const sub = relay.subscribe({ kinds: [1], since: now() });
 
 for await (const event of sub.events) {
   console.log(event);
