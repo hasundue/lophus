b2b4afcb897e527de73205b7bc39a95c580b9aa9
hasundue <hasundue@gmail.com>
2023-05-22 16:31:39 +0900

    Refactor lib/env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -6,31 +6,25 @@ class Env {
   #pubkey?: PublicKey;
 
   get PRIVATE_KEY() {
-    if (this.#nsec) {
-      return this.#nsec;
-    }
-    const value = Deno.env.get("PRIVATE_KEY");
-    if (!value) {
-      throw new Error("PRIVATE_KEY is not set");
-    }
     if (!this.#nsec) {
+      const value = Deno.env.get("PRIVATE_KEY");
+      if (!value) {
+        throw new Error("PRIVATE_KEY is not set");
+      }
       this.#nsec = value as PrivateKey;
     }
-    return value as PrivateKey;
+    return this.#nsec;
   }
 
   get PUBLIC_KEY() {
     if (this.#pubkey) {
-      return this.#pubkey;
-    }
-    const value = Deno.env.get("PUBLIC_KEY");
-    if (!value) {
-      throw new Error("PUBLIC_KEY is not set");
-    }
-    if (!this.#pubkey) {
+      const value = Deno.env.get("PUBLIC_KEY");
+      if (!value) {
+        throw new Error("PUBLIC_KEY is not set");
+      }
       this.#pubkey = value as PublicKey;
     }
-    return value as PublicKey;
+    return this.#pubkey;
   }
 }
 
