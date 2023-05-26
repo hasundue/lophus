1f736f4c3f95e5dcb265c7ee55d45bf5555c93e8
hasundue <hasundue@gmail.com>
2023-05-26 18:47:53 +0900

    refactor(core/streams): implement NonExclusiveWritableStream

--- core/streams.ts
+++ core/streams.ts
@@ -21,3 +21,80 @@ export function broadcast<T = unknown>(
     }),
   );
 }
+
+/**
+ * Merge multiple streams into one.
+ */
+export function merge<R extends unknown>(
+  streams: ReadableStream<R>[],
+) {
+  const readers = streams.map((st) => st.getReader());
+  return new ReadableStream<R>({
+    pull(controller) {
+      Promise.race(readers.map(async (r) => {
+        const { value, done } = await r.read();
+        if (value) controller.enqueue(value);
+        if (done) throw done;
+      }));
+    },
+  });
+}
+
+export class NonExclusiveWritableStream<W = unknown>
+  implements WritableStream<W> {
+  readonly locked = false;
+
+  #aggregator: WritableStream<W>;
+  #channels = new Map<string, ReadableStream<W>>();
+
+  constructor(
+    protected underlyingSink: UnderlyingSink<W>,
+    protected strategy?: QueuingStrategy<W>,
+  ) {
+    this.#aggregator = this.#update();
+  }
+
+  #update() {
+    this.#aggregator = new WritableStream<W>({
+      write: (chunk, controller) => {
+        this.underlyingSink.write?.(chunk, controller);
+      },
+      close: this.underlyingSink.close,
+      abort: this.underlyingSink.abort,
+    }, this.strategy);
+
+    merge(Array.from(this.#channels.values())).pipeTo(this.#aggregator);
+
+    return this.#aggregator;
+  }
+
+  getWriter() {
+    const key = crypto.randomUUID();
+    const channel = new TransformStream<W, W>({
+      flush: (controller) => {
+        controller.terminate();
+        this.#channels.delete(key);
+        this.#update();
+      },
+    });
+    this.#channels.set(key, channel.readable);
+    this.#update();
+    return channel.writable.getWriter();
+  }
+
+  // TODO: Make this parallel
+  async abort() {
+    for (const ch of this.#channels.values()) {
+      await ch.cancel();
+    }
+    await this.#aggregator.abort();
+  }
+
+  // TODO: Make this parallel
+  async close() {
+    for (const ch of this.#channels.values()) {
+      await ch.cancel();
+    }
+    await this.#aggregator.close();
+  }
+}
