19a42ff9e96a097bdf3135f439d6df3224fc9070
hasundue <hasundue@gmail.com>
2023-06-09 15:17:06 +0900

    chore(lib/x/streamtools): remove pipeThroughFrom

--- lib/x/streamtools.ts
+++ lib/x/streamtools.ts
@@ -1,3 +1,2 @@
-export { pipeThroughFrom } from "https://deno.land/x/streamtools@v0.5.0/pipe_through_from.ts";
 export { collect } from "https://deno.land/x/streamtools@v0.5.0/collect.ts";
 export { pop } from "https://deno.land/x/streamtools@v0.5.0/pop.ts";
