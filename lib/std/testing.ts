0267454864833b7c66ef39e7460d79b937c01e9e
hasundue <hasundue@gmail.com>
2023-09-29 20:43:44 +0900

    refactor(lib/std/testing): remove unused exports

--- lib/std/testing.ts
+++ lib/std/testing.ts
@@ -1,11 +1,4 @@
 export {
-  assert,
-  assertArrayIncludes,
-  assertEquals,
-  assertFalse,
-  assertObjectMatch,
-} from "https://deno.land/std@0.203.0/testing/asserts.ts";
-export {
   afterAll,
   afterEach,
   beforeAll,
