d99136f37e8115d245b3984a8c5db0adf218795e
hasundue <hasundue@gmail.com>
2023-09-29 10:19:22 +0900

    docs(examples): use ConsoleLogger in examples

--- docs/src/examples/pool.ts
+++ docs/src/examples/pool.ts
@@ -1,7 +1,8 @@
 // Stream from multiple relays with a relay pool
 import { RelayPool } from "../../../lib/pools.ts";
 import { Timestamp } from "../../../lib/times.ts";
+import { ConsoleLogger } from "../../../lib/logging.ts";
 
 new RelayPool("wss://nos.lol", "wss://relay.nostr.band")
   .subscribe({ kinds: [1], since: Timestamp.now })
-  .pipeTo(new WritableStream({ write: (ev) => console.log(ev) }));
+  .pipeTo(new ConsoleLogger());
