4768cf129200c7507d724e17a11c87332b408ef5
hasundue <hasundue@gmail.com>
2023-10-13 09:59:56 +0900

    "build(deps): update deno.land/x/emit from 0.22.0 to 0.29.0"

--- bin/bundle.ts
+++ bin/bundle.ts
@@ -1,4 +1,4 @@
-import { bundle } from "https://deno.land/x/emit@0.22.0/mod.ts";
+import { bundle } from "https://deno.land/x/emit@0.29.0/mod.ts";
 import { transform } from "https://deno.land/x/esbuild@v0.19.4/mod.js";
 
 const result = await bundle(
