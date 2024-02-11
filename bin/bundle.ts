256a33ecdcf49785cf2f63b7eecd6abac94514c2
hasundue <hasundue@gmail.com>
2024-02-11 09:12:29 +0900

    build(deps): bump deno.land/x/esbuild from v0.19.4 to v0.20.0

--- bin/bundle.ts
+++ bin/bundle.ts
@@ -1,5 +1,5 @@
 import { bundle } from "https://deno.land/x/emit@0.36.0/mod.ts";
-import { transform } from "https://deno.land/x/esbuild@v0.19.4/mod.js";
+import { transform } from "https://deno.land/x/esbuild@v0.20.0/mod.js";
 
 const result = await bundle(
   new URL("../core/relays.ts", import.meta.url),
