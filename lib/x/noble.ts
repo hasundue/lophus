4bb195d3ee571d1dd103a4dbc8c9985cce23175d
hasundue <hasundue@gmail.com>
2024-02-11 09:12:12 +0900

    build(deps): bump @noble/curves from 1.2.0 to 1.3.0

--- lib/x/noble.ts
+++ lib/x/noble.ts
@@ -1,3 +1,3 @@
-export { schnorr } from "npm:@noble/curves@1.2.0/secp256k1";
+export { schnorr } from "npm:@noble/curves@1.3.0/secp256k1";
 export { sha256 } from "npm:@noble/hashes@1.3.2/sha256";
 export { bytesToHex } from "npm:@noble/hashes@1.3.2/utils";
