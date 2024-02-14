c2b4f76ee79e5de09986a7575f4cad81fe205fc8
hasundue <hasundue@gmail.com>
2024-02-14 16:47:29 +0900

    implement `addModule` in nodes

--- core/nodes.ts
+++ core/nodes.ts
@@ -2,13 +2,19 @@ import type { NostrMessage } from "./protocol.d.ts";
 import type { Logger } from "./types.ts";
 import { WebSocketLike } from "./websockets.ts";
 
-export interface NostrNodeConfig {
-  modules: NostrNodeModule[];
+export interface NostrNodeConfig<
+  W extends NostrMessage = NostrMessage,
+  R extends EventTypeRecord = EventTypeRecord,
+> {
+  modules: NostrNodeModule<W, R>[];
   logger: Logger;
   nbuffer: number;
 }
 
-export type NostrNodeOptions = Partial<NostrNodeConfig>;
+export type NostrNodeOptions<
+  W extends NostrMessage = NostrMessage,
+  R extends EventTypeRecord = EventTypeRecord,
+> = Partial<NostrNodeConfig<W, R>>;
 
 /**
  * Common base class for relays and clients.
@@ -19,17 +25,18 @@ export class NostrNode<
 > extends WritableStream<W> implements EventTarget {
   readonly #eventTarget = new EventTarget();
   readonly #aborter = new AbortController();
-  readonly config: Readonly<NostrNodeConfig>;
+  readonly config: Readonly<NostrNodeConfig<W, R>>;
 
   constructor(
     readonly ws: WebSocketLike,
-    opts: NostrNodeOptions = {},
+    opts: NostrNodeOptions<W, R> = {},
   ) {
     super({
       write: (msg) => this.ws.send(JSON.stringify(msg)),
       close: () => this.ws.close(),
     });
     this.config = { modules: [], logger: {}, nbuffer: 10, ...opts };
+    this.config.modules.forEach(this.addModule);
   }
 
   send(msg: W) {
@@ -51,7 +58,7 @@ export class NostrNode<
     }
   }
 
-  install = (module: NostrNodeModule<W, R>) => module.default(this);
+  addModule = (module: NostrNodeModule<W, R>) => module.default(this);
 
   addEventListener = <T extends EventType<R>>(
     type: T,
