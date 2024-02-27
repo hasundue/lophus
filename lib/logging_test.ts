06b3912be858855da3a2aedff61997beb6596014
hasundue <hasundue@gmail.com>
2024-02-27 13:47:18 +0900

    test(lib/logging): stub `console`

--- lib/logging_test.ts
+++ lib/logging_test.ts
@@ -1,15 +1,29 @@
-import { describe, it } from "../lib/std/testing.ts";
+import { beforeEach, describe, it } from "../lib/std/testing.ts";
+import { assertEquals } from "../lib/std/assert.ts";
 import { ConsoleLogger } from "./logging.ts";
 import { assert } from "../lib/std/assert.ts";
 
 describe("ConsoleLogger", () => {
+  let output = "";
+
+  beforeEach(() => {
+    globalThis.console = new Proxy(globalThis.console, {
+      get: () => {
+        return (data: unknown) => {
+          output += data;
+        };
+      },
+    });
+  });
+
   it("should be a writable stream", () => {
     const logger = new ConsoleLogger();
     assert(logger instanceof WritableStream);
   });
+
   it("should log to console", async () => {
-    // TODO: Stub console.log
     const logger = new ConsoleLogger();
     await logger.getWriter().write("ConsoleLogger");
+    assertEquals(output, "ConsoleLogger");
   });
 });
