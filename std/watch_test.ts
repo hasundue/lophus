08088d161c47e0e45317138954235f83f388d338
hasundue <hasundue@gmail.com>
2024-03-22 15:33:38 +0900

    feat(std): `watch` for websockets

--- std/watch_test.ts
+++ std/watch_test.ts
@@ -4,7 +4,46 @@ import { MockWebSocket } from "@lophus/lib/testing";
 import { Relay } from "@lophus/nips/relays";
 import { watch } from "./watch.ts";
 
-describe("watch - Relay", () => {
+describe("watch - websockets", () => {
+  const ws = new MockWebSocket();
+
+  it("should create a chainable from a websocket", () => {
+    const chainable = watch(ws);
+    assertExists(chainable.call);
+  });
+
+  it("should create a stream of events from multiple websockets", () => {
+    const chainable = watch(ws, ws);
+    assertExists(chainable.call);
+  });
+
+  it("should create a stream of events from a websocket", () => {
+    const stream = watch(ws)("message");
+    assertInstanceOf(stream, ReadableStream);
+  });
+
+  it("should create a stream of events of multiple types from a websocket", () => {
+    const stream = watch(ws)("message", "open");
+    assertInstanceOf(stream, ReadableStream);
+  });
+
+  it("should create a stream of events of multiple types from multiple websockets", () => {
+    const stream = watch(ws, ws)("message", "open");
+    assertInstanceOf(stream, ReadableStream);
+  });
+
+  it("should receive an event from a websocket", async () => {
+    const stream = watch(ws)("message");
+    const reader = stream.getReader();
+    ws.dispatchEvent(new MessageEvent("message", { data: "test" }));
+    const { value } = await reader.read();
+    assertExists(value);
+    assertEquals(value.type, "message");
+    assertEquals(value.data, "test");
+  });
+});
+
+describe("watch - relays", () => {
   let relay: Relay;
 
   beforeAll(() => {
