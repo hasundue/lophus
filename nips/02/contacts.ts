7f4a2f8c350db534a645ef282d2ba01fd26dbbb9
hasundue <hasundue@gmail.com>
2024-03-25 19:40:40 +0900

    fix(nips/02): make `parse` throw TypeError for non-"p" tags

--- nips/02/contacts.ts
+++ nips/02/contacts.ts
@@ -8,6 +8,9 @@ export interface Contact {
 }
 
 export function parse(tag: ContactTag): Contact {
+  if (tag[0] !== "p") {
+    throw new TypeError("Expected a contact tag", { cause: tag });
+  }
   return {
     pubkey: tag[1],
     relay: tag[2],
--- nips/02/contacts_test.ts
+++ nips/02/contacts_test.ts
@@ -1,22 +1,29 @@
+import { assertObjectMatch, assertThrows } from "@std/assert";
 import { describe, it } from "@std/testing/bdd";
 import { PublicKey, RelayUrl } from "@lophus/core/protocol";
 import { parse } from "./contacts.ts";
-import { assertObjectMatch } from "@std/assert";
 
 describe("Contact - parse", () => {
   it("should parse a contact object from a contact tag", () => {
     assertObjectMatch(
       parse([
         "p",
-        "npub..." as PublicKey,
+        "abcde..." as PublicKey,
         "wss://localhost:8080" as RelayUrl,
         "Alice",
       ]),
       {
-        pubkey: "npub...",
+        pubkey: "abcde...",
         relay: "wss://localhost:8080",
         petname: "Alice",
       },
+    );
+  });
+
+  it("should throw a TypeError if the tag is not a contact", () => {
+    assertThrows(
+      // @ts-expect-error Expected a contact tag
+      () => parse(["t", "nostrdev"]),
     );
   });
 });
